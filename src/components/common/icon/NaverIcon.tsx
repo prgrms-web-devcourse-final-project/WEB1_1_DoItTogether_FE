d0123d95d13eb3502ad41de0ac6d82cf4f92966b
Commeci <cjtkemfldj@naver.com>
2025-01-19 11:58:41 +0900

    ➕ Add: 구글, 네이버, 카카오 아이콘 추가가

---
+++ src/components/common/icon/NaverIcon.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+interface NaverIconProps {}
+
+const NaverIcon: React.FC<NaverIconProps> = ({}) => {
+  return (
+    <svg width='16' height='16' viewBox='0 0 16 16' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <g clipPath='url(#clip0_3370_17086)'>
+        <path
+          d='M10.8491 8.56267L4.91687 0H0V16H5.15088V7.436L11.0831 16H16V0H10.8491V8.56267Z'
+          fill='white'
+        />
+      </g>
+      <defs>
+        <clipPath id='clip0_3370_17086'>
+          <rect width='16' height='16' fill='white' />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
+
+export default NaverIcon;
