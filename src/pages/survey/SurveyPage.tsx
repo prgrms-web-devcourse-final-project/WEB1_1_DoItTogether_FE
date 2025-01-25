c2344a49ca5d520f96f4587ee84783a71f3fe75a
yook <si89336@naver.com>
2025-01-26 00:23:32 +0900

    ♻️ Refactor: 퍼널 패턴 리팩토링

--- src/pages/survey/SurveyPage.tsx
+++ src/pages/survey/SurveyPage.tsx
@@ -1,28 +1,20 @@
-import React from 'react';
 import { motion } from 'framer-motion';
 import BackBtn from '@/components/common/button/BackBtn/BackBtn';
 import { Progress } from '@/components/common/ui/progress';
-import { Button } from '@/components/common/ui/button';
 import { Step1, Step2, Step3, Step4, Step5, LoadingScreen } from '@/components/survey';
-import { useSurvey } from '@/hooks/useSurvey';
 import MetaTags from '@/components/common/metaTags/MetaTags';
-import { BUTTON_TEXT } from '@/constants/onBoarding';
+import {
+  DUMMY_QUESTION_STEP1,
+  DUMMY_QUESTION_STEP2,
+  DUMMY_QUESTION_STEP3,
+  DUMMY_QUESTION_STEP4,
+} from '@/constants/onBoarding';
+import useFunnel from '@/hooks/useFunnel';
+import useLoadingState from '@/hooks/survey/useLoadingState';
+import useUserName from '@/hooks/survey/useUserName';
+import useSurveyState from '@/hooks/survey/useSurveyState';
 
-const SurveyPage: React.FC = () => {
-  const {
-    step,
-    progressStep,
-    isCompleted,
-    result,
-    username,
-    loading,
-    setNextStep,
-    setPrevStep,
-    handleAnswer,
-    isStepValid,
-    stepComponents,
-  } = useSurvey();
-
+const SurveyPage = () => {
   const item = {
     hidden: { opacity: 0 },
     show: {
@@ -34,44 +26,21 @@ const SurveyPage: React.FC = () => {
     },
   };
 
-  const isValid = isStepValid();
+  const [Funnel, setStep, currentStep] = useFunnel<
+    '첫번째' | '두번째' | '세번째' | '네번째' | '설문결과'
+  >('첫번째');
 
-  const renderStep = () => {
-    if (step <= 4) {
-      const StepComponent = [Step1, Step2, Step3, Step4][step - 1];
-      const { questions, title } = stepComponents[step - 1];
-      return <StepComponent title={title} questions={questions} handleAnswer={handleAnswer} />;
-    }
-    return (
-      <div className='h-full pt-28'>
-        <Step5 title={`${username}님의 청소성향은`} results={result} />
-      </div>
-    );
-  };
-
-  const renderContent = () => (
-    <motion.div
-      className='flex flex-1 flex-col gap-8 px-5'
-      variants={item}
-      initial='hidden'
-      animate='show'
-    >
-      {renderStep()}
-    </motion.div>
-  );
-
-  const renderButton = () => (
-    <motion.div className='sticky bottom-6 bg-white px-5'>
-      <Button
-        size={'large'}
-        variant={!isValid ? 'disabled' : 'full'}
-        onClick={setNextStep}
-        disabled={!isValid}
-      >
-        {step === 5 ? BUTTON_TEXT.COMPLETE : BUTTON_TEXT.NEXT}
-      </Button>
-    </motion.div>
-  );
+  //로딩 상태 관리 커스텀 훅
+  const { isLoading, isCompleted, setIsLoading, setIsCompleted } = useLoadingState();
+  //사용자 이름을 가져오는 커스텀 훅
+  const { username } = useUserName(currentStep);
+  //설문을 관리하는 커스텀 훅
+  const { progressStep, result, handleNextStep, handleDone, handlePrevStep } = useSurveyState({
+    currentStep,
+    setStep,
+    setIsLoading,
+    setIsCompleted,
+  });
 
   return (
     <div className={`flex h-screen flex-col`}>
@@ -80,23 +49,95 @@ const SurveyPage: React.FC = () => {
         description={'나의 집안일 스타일을 알아보세요.'}
         url={`https://doit-together.vercel.app/survey/`}
       />
-      {step <= 4 && (
-        <motion.div variants={item} initial='hidden' animate='show'>
-          <div className='p-5'>
-            <BackBtn handleClick={setPrevStep} />
-          </div>
-          <Progress value={(progressStep / 5) * 100} className='mb-8' />
-        </motion.div>
+
+      {currentStep !== '설문결과' && (
+        <>
+          {/* 헤더 */}
+          <motion.div variants={item} initial='hidden' animate='show'>
+            <div className='p-5'>
+              <BackBtn handleClick={handlePrevStep} />
+            </div>
+            <Progress value={(progressStep / 5) * 100} className='mb-8' />
+          </motion.div>
+        </>
       )}
 
-      {loading ? (
-        <div className='h-full px-0'>
+      {isLoading ? (
+        <>
           <LoadingScreen username={username} isCompleted={isCompleted} />
-        </div>
+        </>
       ) : (
         <>
-          {renderContent()}
-          {renderButton()}
+          {/* 본문 */}
+          <Funnel>
+            <Funnel.Step name='첫번째'>
+              <motion.div
+                className='flex flex-1 flex-col gap-8 px-5'
+                variants={item}
+                initial='hidden'
+                animate='show'
+              >
+                <Step1
+                  title={`평소 정리정돈에 대해\n어떻게 생각하시나요?`}
+                  questions={DUMMY_QUESTION_STEP1}
+                  handleNextStep={handleNextStep}
+                />
+              </motion.div>
+            </Funnel.Step>
+            <Funnel.Step name='두번째'>
+              <motion.div
+                className='flex flex-1 flex-col gap-8 px-5'
+                variants={item}
+                initial='hidden'
+                animate='show'
+              >
+                <Step2
+                  title={`어떤 방식으로 일하는 것을\n선호하시나요?`}
+                  questions={DUMMY_QUESTION_STEP2}
+                  handleNextStep={handleNextStep}
+                />
+              </motion.div>
+            </Funnel.Step>
+            <Funnel.Step name='세번째'>
+              <motion.div
+                className='flex flex-1 flex-col gap-8 px-5'
+                variants={item}
+                initial='hidden'
+                animate='show'
+              >
+                <Step3
+                  title={`주변 환경이\n작업에 얼마나 영향을 주나요?`}
+                  questions={DUMMY_QUESTION_STEP3}
+                  handleNextStep={handleNextStep}
+                />
+              </motion.div>
+            </Funnel.Step>
+            <Funnel.Step name='네번째'>
+              <motion.div
+                className='flex flex-1 flex-col gap-8 px-5'
+                variants={item}
+                initial='hidden'
+                animate='show'
+              >
+                {progressStep !== 5 && (
+                  <Step4
+                    title={`집안일을 할 때\n어떤 감정을 느끼시나요?`}
+                    questions={DUMMY_QUESTION_STEP4}
+                    handleNextStep={handleNextStep}
+                  />
+                )}
+              </motion.div>
+            </Funnel.Step>
+            <Funnel.Step name='설문결과'>
+              <div className='flex flex-1 flex-col gap-8 px-5'>
+                <Step5
+                  title={`${username}님의 청소성향은`}
+                  results={result}
+                  handleDone={handleDone}
+                />
+              </div>
+            </Funnel.Step>
+          </Funnel>
         </>
       )}
     </div>
