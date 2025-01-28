f7d57e7ec3e70175bb333750374be8ebc8ce8653
Eugeeun <jack603@naver.com>
2025-01-28 16:21:13 +0900

    🔧 Wrench: 찌르기, 칭찬 알림 API 함수 수정

--- src/services/noticeManage/postPoke.ts
+++ src/services/noticeManage/postPoke.ts
@@ -1,10 +1,15 @@
 import { axiosInstance } from '@/services/axiosInstance';
 import { PokeReq, PokeRes } from '@/types/apis/noticeManage';
 
-export const postPoke = async ({ channelId, targetUserId, reactDate }: PokeReq) => {
+export const postPoke = async ({
+  channelId,
+  targetUserId,
+  reactDate,
+  notificationRequest,
+}: PokeReq) => {
   const response = await axiosInstance.post<PokeRes>(
     `/api/v1/channels/${channelId}/reactions/poke`,
-    { targetUserId, reactDate }
+    { targetUserId, reactDate, notificationRequest }
   );
   return response.data;
 };
