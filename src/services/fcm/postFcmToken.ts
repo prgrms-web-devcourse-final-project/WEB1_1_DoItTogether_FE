efa7ed8be5c29821581c32ba3a340a1c96781cfe
Eugeeun <jack603@naver.com>
2025-01-28 22:44:52 +0900

    🔧 Wrench: 함수 이름 수정

--- src/services/fcm/postFcmToken.ts
+++ src/services/fcm/postFcmToken.ts
@@ -1,7 +1,7 @@
 import { axiosInstance } from '@/services/axiosInstance';
 import { PostTokenToServerReq, PostTokenToServerRes } from '@/types/apis/fcmApi';
 
-export const postFcmPush = async ({ token, platformType }: PostTokenToServerReq) => {
+export const postFcmToken = async ({ token, platformType }: PostTokenToServerReq) => {
   const response = await axiosInstance.post<PostTokenToServerRes>(`/api/v1/fcms/token`, {
     token,
     platformType,
