7fd966d88480755087001083d01388212608e00c
ayoung <a01259@naver.com>
2025-01-30 00:29:28 +0900

    ✨ Feat: 디바이스타입 전역 store 와 디바이스 확인할 훅을 생성하여 app 에서 호출

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { router } from '@/router';
 import { RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import useDevice from '@/hooks/useDevice';
 
 const queryClient = new QueryClient();
 
 function App() {
+  useDevice();
+
   return (
     <>
       <QueryClientProvider client={queryClient}>
