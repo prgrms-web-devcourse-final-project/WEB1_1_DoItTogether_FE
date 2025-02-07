eded843e8e4c6f05f404e3f12263f50a7a9791b1
Commeci <cjtkemfldj@naver.com>
2025-02-07 15:28:51 +0900

    ➕ Add: LoginButtons 컴포넌트 추가

---
+++ src/components/landing/LoginButtons.tsx
@@ -0,0 +1,20 @@
+import LoginButton from '@/components/landing/LoginButton';
+
+interface LoginButtonsProps {
+  handleLogin: (provider: 'kakao' | 'naver' | 'google') => void;
+}
+
+const LoginButtons = ({ handleLogin }: LoginButtonsProps) => {
+  return (
+    <div className='sticky bottom-12 flex flex-col gap-4 pt-3'>
+      <div className='text-gray3 font-label'>—— SNS 간편 로그인 ——</div>
+      <div className='flex justify-center gap-6'>
+        <LoginButton provider='kakao' handleLoginButton={() => handleLogin('kakao')} />
+        <LoginButton provider='naver' handleLoginButton={() => handleLogin('naver')} />
+        <LoginButton provider='google' handleLoginButton={() => handleLogin('google')} />
+      </div>
+    </div>
+  );
+};
+
+export default LoginButtons;
